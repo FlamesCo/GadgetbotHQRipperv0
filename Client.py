5a55a73be9e87ed06c6be440598eeb9e27728622
Flames Elite <84363801+FlamesLLC@users.noreply.github.com>
2021-11-29 17:33:56 -0800

    Gadgetbot.v01a
    THE BETAMIX VERSION 0 NOV 29 MONDAY 5:33 FST

---
+++ Client.py
@@ -0,0 +1,8 @@
+## Gadget bot is a toolkit that automates youtube video editing and music editing for timmyturnergrandad and silvagunner remixes.
+import os
+import sys
+import requests
+import re 
+import time
+ 
+ print("Welcome to the ripper bot.")
